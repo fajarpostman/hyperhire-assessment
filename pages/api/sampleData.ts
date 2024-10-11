c7e5ea47941160c5c49c75959ced30cbcfe2aa2b
fajarpostman <fajardwirianto3@gmail.com>
2024-10-12 03:11:08 +0700

    update sampledata

--- pages/api/sampleData.ts
+++ pages/api/sampleData.ts
@@ -7,6 +7,22 @@ export const sampleData = [
         skills: ["마케팅 콘텐츠 제작", "인스타그램 관리", "트위터 관리", "블로그 글 작성"],
         salary: "100만원",
         imgSrc: "/images/profile-pic.png"
+    },
+    {
+        name: "Abhisek Gupta",
+        job: "마케팅",
+        experience: "2y+",
+        skills: ["마케팅 콘텐츠 제작", "인스타그램 관리", "트위터 관리", "블로그 글 작성"],
+        salary: "100만원",
+        imgSrc: "/images/profile-pic.png"
+    },
+    {
+        name: "Abhisek Gupta",
+        job: "마케팅",
+        experience: "2y+",
+        skills: ["마케팅 콘텐츠 제작", "인스타그램 관리", "트위터 관리", "블로그 글 작성"],
+        salary: "100만원",
+        imgSrc: "/images/profile-pic.png"
     }
 ];
 
